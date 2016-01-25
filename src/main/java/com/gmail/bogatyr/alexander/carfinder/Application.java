ae55da7d3d6cc8a24e326480564fbd7b5d9bed61
Alexander Bogatyrenko <alexander.bogatyr@gmail.com>
2016-01-26 00:16:50 +0300

    added simple rest controller

---
+++ src/main/java/com/gmail/bogatyr/alexander/carfinder/Application.java
@@ -0,0 +1,15 @@
+package com.gmail.bogatyr.alexander.carfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by alexander on 26.01.16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
